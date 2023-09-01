e46cabcc7cafc8c6daec06062efd1539bf38474c
PavelDrozd <87943574+PavelDrozd@users.noreply.github.com>
2023-09-01 17:43:29 +0300

    Add javadoc in exceptions

--- src/main/java/com/clevertec/bankmanager/shared/exception/store/dao/connection/ConnectionException.java
+++ src/main/java/com/clevertec/bankmanager/shared/exception/store/dao/connection/ConnectionException.java
@@ -2,24 +2,81 @@ package com.clevertec.bankmanager.shared.exception.store.dao.connection;
 
 import com.clevertec.bankmanager.shared.exception.store.dao.DaoException;
 
+/**
+ * Thrown to indicate that an error has occurred during a connection.
+ *
+ * @since 1.0
+ */
 public class ConnectionException extends DaoException {
 
+    /**
+     * Constructs a new exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
     public ConnectionException() {
         super();
     }
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
     public ConnectionException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
     public ConnectionException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a new exception with the specified cause and a
+     * detail message of {@code (cause==null ? null : cause.toString())}
+     * (which typically contains the class and detail message of
+     * {@code cause}).  This constructor is useful for runtime exceptions
+     * that are little more than wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A {@code null} value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     * @since 1.4
+     */
     public ConnectionException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a new exception with the specified detail
+     * message, cause, suppression enabled or disabled, and writable
+     * stack trace enabled or disabled.
+     *
+     * @param message            the detail message.
+     * @param cause              the cause.  (A {@code null} value is permitted,
+     *                           and indicates that the cause is nonexistent or unknown.)
+     * @param enableSuppression  regardless of whether suppression is enabled or disabled or not
+     * @param writableStackTrace should the stack trace be writable or not
+     * @since 1.7
+     */
     protected ConnectionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
